ea669d3e7cee862b43f89dc1e2df22b91d5ac7f8
Nick Anthony Miras <mirasnickanthony@gmail.com>
2025-08-07 17:31:26 +0800

    deleted test files and added source file for disconnect protocol

---
+++ lib/execute/src/disconnectProtocol.h
@@ -0,0 +1,9 @@
+//
+// Created by Nick Anthony Miras on 8/7/25.
+//
+// this code contains the functions to run when slave gets disconnected
+
+#ifndef SLAVEEXECUTE_H
+#define SLAVEEXECUTE_H
+
+#endif //SLAVEEXECUTE_H
